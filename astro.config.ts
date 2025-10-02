2516a49aa449d75f8e8b3cd34ca00f1f776b6f38
Ric <henriqueft04@ua.pt>
2025-10-02 20:17:30 +0100

    revert

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,25 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 import { defineConfig } from 'astro/config';
+
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
+import compress from 'astro-compress';
+import type { AstroIntegration } from 'astro';
+
 import astrowind from './vendor/integration';
 
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const hasExternalScripts = false;
+const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroIntegration)[] = []) =>
+  hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
   output: 'static',
@@ -34,16 +46,51 @@ export default defineConfig({
         ],
       },
     }),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ),
+
+    compress({
+      CSS: true,
+      HTML: {
+        'html-minifier-terser': {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }),
+
     astrowind({
       config: './src/config.yaml',
     }),
   ],
+
+  image: {
+    domains: ['cdn.pixabay.com'],
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
+
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+  },
 
   vite: {
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './src'),
       },
+    },
+    build: {
+      assetsInlineLimit: 0,
     },
   },
 });
